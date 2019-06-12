500913610c197c4858b1149ea739960bd303f0fe
Sergio Heynes <sergio.heynes@gmail.com>
2019-06-12 10:56:15 -0500

    Get and set values in local storage finished.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -52,8 +52,61 @@ function setBgGreet(){
     }
 }
 
+// Get name
+function getName() {
+    if(localStorage.getItem('name') === null) {
+        name.textContent = '[Enter Name]';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
+}
+
+// Set name
+function setName(e){
+    if(e.type === 'keypress') {
+        // Make sure enter is press
+        if(e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('name', e.target.innerText);
+            name.blur();
+        }
+    } else {
+        localStorage.setItem('name', e.target.innerText);
+    }
+}
+
+// Get focus
+function getFocus() {
+    if(localStorage.getItem('focus') === null) {
+        focus.textContent = '[Enter Focus]';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
+// Set focus
+function setFocus(e){
+    if(e.type === 'keypress') {
+        // Make sure enter is press
+        if(e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('focus', e.target.innerText);
+            focus.blur();
+        }
+    } else {
+        localStorage.setItem('focus', e.target.innerText);
+    }
+}
+
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+
+
+
 // Run
 showTime();
 setBgGreet();
-
+getName();
+getFocus();
 
